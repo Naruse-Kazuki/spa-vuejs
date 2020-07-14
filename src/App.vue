1c26b7daaa31a156a94094191bea52806be13d65
Naruse-Kazuki <kuroraido@icloud.com>
2020-07-14 11:42:12 +0900

    section14

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <router-view name="header"></router-view>
+    <transition name="fade" mode="out-in" @before-enter="beforeEnter">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    beforeEnter() {
+      this.$root.$emit("triggerScroll");
+    }
+  }
+};
+</script>
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+</style>
